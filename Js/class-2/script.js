08cd29f612f9078786ac055faa83b1c866e7c7ba
Asif-ak <iu.asifak@gmail.com>
2019-04-17 20:30:15 +0500

    js class 2

--- Js/class-2/script.js
+++ Js/class-2/script.js
@@ -1,5 +1,5 @@
-var name=prompt("Your Name");
-var age =+prompt("age? ");
+// var name=prompt("Your Name");
+// var age =+prompt("age? ");
 
 // in js = is assignment operator
 // == comparison operator
@@ -7,10 +7,37 @@ var age =+prompt("age? ");
 
 // in below example, because prompt is sending 55  as string
 // we need to type cast it using '+' with prompt
-if (name !== "Osama" && age === 55){
-    alert("Welcome "+name);
-}
 
-else { 
-    alert("get Lost");
-}
+// if (name !== "Osama" && age === 55){
+//     alert("Welcome "+name);
+// }
+
+// else if (name == "Osama" && age === 55){
+//     alert("Welcome "+name);
+// }
+
+// if (age > 10 && age < 15){
+//     alert("eligible");
+// }
+// else { 
+//     alert("get Lost");
+// }
+
+var cities =["karachi","Lahore","Multan",null,undefined];
+console.log(cities);
+console.log(cities.length);
+// console.log(cities[4]);
+cities.push(345);
+console.log(cities);
+cities[0]="hyderabad";
+// delete cities[2]; // dont use it
+cities.pop();
+console.log(cities);
+
+
+cities.splice(1,0,[1,2,3]);
+console.log(cities);
+console.log(cities[1][1]);
+cities[1].forEach(element => {
+    console.log(element);
+});
