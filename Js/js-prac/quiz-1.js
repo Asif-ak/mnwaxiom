3a8115eeb4c58bf67940c597ef44f40b7d8e65a6
Asif-ak <iu.asifak@gmail.com>
2019-05-19 01:49:39 +0500

    js prac for exam 1

---
+++ Js/js-prac/quiz-1.js
@@ -0,0 +1,42 @@
+// chapter 1
+// alert wont work without html. use console.log instead
+// alert("Hello World");
+console.log('Hello World')
+
+// chapter 2
+var name ="Mark";
+console.log(name);
+name='Ace';
+console.log(name);
+
+var nat;
+nat = 'U.S.';
+console.log(nat);
+
+// alert(nat)
+
+//chapter 3
+
+var weight =150;
+weight=weight+55;
+console.log(weight);
+
+var num = "20";
+var newnum = 20;
+console.log(num+newnum);
+
+//var 1pres ="abc"; // that variable declaration is illegal
+var first1pres="def"; // that is legal
+
+console.log(newnum);
+
+console.log(typeof(newnum));
+
+// alerting the number will convert it to string
+
+// chapter 4
+// naming conventions
+// 1 can contain : letter, number, underscore, dollarsign, js keywords
+// 2 cannot contain: spaces 
+// 3 cannot start with: number
+// 4 use camelcase e.g newNumber, somethingImportant, codeRunner
