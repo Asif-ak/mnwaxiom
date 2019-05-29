fc0ce0b012828ac073409079b7b13725f0ebc8e2
Asif-ak <iu.asifak@gmail.com>
2019-05-29 19:04:13 +0500

    js assignment node

--- Js/assignment/nodes/nodes.js
+++ Js/assignment/nodes/nodes.js
@@ -2,29 +2,20 @@ function addli(){
     var parent =document.getElementById("ul1");
     var li=document.createElement("li");
     
-    var text=document.getElementsByTagName("input")[0].value;
+    var text=document.getElementById("text-1").value;
     var btton = document.createElement("input");
     btton.setAttribute("type","submit");
     btton.setAttribute("value","Delete")
-    btton.setAttribute("onclick","removeli();")
-    // btton.onclick=function() {
-    //    console.log(btton.parentNode.removeChild(this)); 
-    // }
+    btton.setAttribute("onclick","removeli(this)")
     li.textContent= text;
-    // li.setAttribute("id",text.)
     li.appendChild(btton);
     parent.appendChild(li);
+    document.getElementById("text-1").value="";
 }
 
-function removeli(){
-    var items = document.getElementsByTagName("li");
-    for (var index = 0; index < items.length; index++) {
-        // console.log(items[index].childNodes[1]);
-        items[index].onclick=function() {
-            // console.log(items[index].childNodes);
-            this.parentNode.removeChild(this);
-        }
-    }
-
+function removeli(a){
+    var pn = a.parentNode.parentNode;
+    var ntd= a.parentNode;
+    pn.removeChild(ntd);
     
 }
