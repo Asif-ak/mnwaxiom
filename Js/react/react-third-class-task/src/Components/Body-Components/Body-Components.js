26a04f11da2c73197245819d88ae7fbca0cb7678
asif <iu.asifak@gmail.com>
2019-07-17 23:14:47 +0500

    third react class task css settings

--- Js/react/react-third-class-task/src/Components/Body-Components/Body-Components.js
+++ Js/react/react-third-class-task/src/Components/Body-Components/Body-Components.js
@@ -1,17 +1,43 @@
 import React from 'react';
+import './Body-Components.css';
 
 export default function BodyComponent(props) {
     return(
-        <div>
-            <p>
-                {props.name}
+        <div className='bodyComponent-container bodyComponent-div-border bodyComponent-default-margin'>
+            <table>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Information
+                </th>
+                <th>
+                    Contact
+                </th>
+                <tr>
+                    <td>
+                        {props.name}
+                    </td>
+                    <td>
+                    {props.info}
+                </td>
+                <td>
+                    {props.contact}
+                    </td>
+                </tr>
+                
+                
+                
+            </table>
+            {/* <p>
+                Name: {props.name}
             </p>
             <p>
-                {props.info}
+                Informations: {props.info}
             </p>
             <p>
-                {props.contact}
-            </p>
+                Contact: {props.contact}
+            </p> */}
         </div>
     )
 }
--- Js/react/react-third-class-task/src/Components/Body/Body.js
+++ Js/react/react-third-class-task/src/Components/Body/Body.js
@@ -6,8 +6,9 @@ export default class Body extends React.Component{
         // set css and use div instead of react.fragment
         return(
             <React.Fragment> 
-                <BodyComponents name="asif" info="Some Info" contact="123456"></BodyComponents>
-                <BodyComponents name="mehreen" info="Other Info" contact="987654"></BodyComponents>
+                <BodyComponents name="Asif" info="Some Info" contact="123456"></BodyComponents>
+                <BodyComponents name="Mehreen" info="Other Info" contact="987654"></BodyComponents>
+                <BodyComponents name="Sohail" info="Various Info" contact="852159"></BodyComponents>
             </React.Fragment>
 
         )
