f7d8c3b358ace83e104c24cf526af1e5f5b1275d
asif <iu.asifak@gmail.com>
2019-07-18 20:40:46 +0500

    react class 4 task

--- Js/react/react-fouth-class/src/ContolledComponent/CC.js
+++ Js/react/react-fouth-class/src/ContolledComponent/CC.js
@@ -5,6 +5,7 @@ export default class UserInput extends React.Component{
     state ={value:"DUmmy"}
 
     changestgate =(e)=>{
+        e.preventDefault(); // to disable default behavior of targeted element
         this.setState({value:e.target.value.toUpperCase()}) // toUpperCase will work because we are recieiveing data in string form
     }
     
@@ -14,10 +15,22 @@ export default class UserInput extends React.Component{
                 <input type='text' onChange={this.changestgate} value={this.state.value} />
                 {/* if we dont want the value of textbox to change we will use value */}
                 {/* if we want the value of textbox to change we will use defaultvalue */}
+                
+                {/* short circiting the value */}
+                {/* {this.state.value && <p>{this.state.value}</p>} */}
 
-                <p>{this.state.value}</p>
+                {/* using turnery operator*/}
+
+                {this.state.value === "ASIF" ? <p><b>{this.state.value}</b></p> : <p>{this.state.value}</p> }
             </form>
         )
+
+        // Create or update the react third class task 
+        // do conditional rendering
+        // create multiple views or pages
+        // and navigate those pages using navbar
+        // dont use actual link or <a> tag
+        // use onclick event instead
     }
 
 
