cb3b68db926e4ba28421156b93d9412ef5812821
asif <iu.asifak@gmail.com>
2019-07-19 23:33:07 +0500

    react class 4 task state done

--- Js/react/react-fourth-class-task/src/Components/Header/Header.js
+++ Js/react/react-fourth-class-task/src/Components/Header/Header.js
@@ -2,9 +2,34 @@ import React from 'react';
 import './Header.css';
 import logo from './logo.svg';
 
-function Header() {
+class Header extends React.Component {
+    constructor(props){
+        super(props);
+        // this.statesetting=this.statesetting.bind(this);
+        // this.li1=React.createRef();
+    }
 
-    // render(){
+    // state={info1:"Info-1", info2:"Info-2", info3:"Info-3"}
+    state={info:""};
+
+    // statesetting=(e)=>{
+    //     const target=e.target;
+    //     const value=target.value;
+    //     e.preventDefault();
+    //     this.setState({info:value});
+    //     // this.setState({info:this.li1.current.value});
+    // }
+
+    // sir amir's solution worked
+
+    handleLiValue = (liValue) =>{
+        this.setState({info: liValue});
+    }
+    
+
+    render(){
+        console.log(this.state.info);
+
         return(
             <div className='header-container'>
                
@@ -15,21 +40,21 @@ function Header() {
                     </div>
                     <div >
                         <ul>
-                            <li>
+                            <li onClick={() => this.handleLiValue("info-1")}>
                                 Info-1
                             </li>
-                            <li>
+                            <li onClick={() => this.handleLiValue("info-2")}>
                                 Info-2
                             </li>
-                            <li>
+                            <li onClick={() => this.handleLiValue("info-3")}>
                                 Info-3
                             </li>
                         </ul>
                     </div>
                     <div >
-                        <p>
+                        <span>
                             <GetDate />
-                        </p>
+                        </span>
 
                     </div>
 
@@ -37,7 +62,9 @@ function Header() {
 
             </div>
         );
-    // }
+    };
+
+    
 }
 function GetDate (){
     let today = new Date();
