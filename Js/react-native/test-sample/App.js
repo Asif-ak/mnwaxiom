12149cd2ab7d2798b106baffe11856d8b42a0585
asif-ak <iu.asifak@gmail.com>
2019-10-03 22:39:10 +0500

    nodejs class 2 and 3 practice

---
+++ Js/react-native/test-sample/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color: 'white', fontSize:30, textAlign: 'center'}}>This is working in android studio emulator</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+});
